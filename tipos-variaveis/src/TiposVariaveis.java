6b2e6e037d321de1ecf11440dd6c86f45dea6650
Gustavo <gustavohmartins208@gmail.com>
2024-09-22 13:58:36 -0300

    atualização

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,28 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 2170333; //Se começar com zero, talez tenha que ser outro tipo
+        long cpf = 974753464L; //Se começar com zero, talez tenha que ser outro tipo. Encerrando com "L" para representar para o java que é literal
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        //TiposEVariaveis
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        //ExemploVariavel
+
+        int numero = 5;
+        System.out.println(numero);
+
+
+
+
+
+
     }
 }
